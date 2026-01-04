7a6952441e31fe38064de8cd503acdd079b65b4d
HurtCore92 <e4255687@gmail.com>
2026-01-04 01:51:34 -0300

    Delete auth.js

--- auth.js
+++
@@ -1,8 +0,0 @@
-const firebaseConfig = { const firebaseConfig = {
-  apiKey: "AIzaSyCuVqt8HcseOGGxHUjuq3gZVg4UfozLnow",
-  authDomain: "hurtcore-e3a43.firebaseapp.com",
-  projectId: "hurtcore-e3a43",
-  storageBucket: "hurtcore-e3a43.firebasestorage.app",
-  messagingSenderId: "753835000234",
-  appId: "1:753835000234:web:f119d43af0a8017d632bd3"
-}; }
