f817c87232424ad373a986ddcfa0bf9731e4dfd3
HurtCore92 <e4255687@gmail.com>
2026-01-03 23:10:01 -0300

    Add files via upload

---
+++ auth.js
@@ -0,0 +1,17 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyCuVqt8HcseOGGxHUjuq3gZVg4UfozLnow",
+  authDomain: "hurtcore-e3a43.firebaseapp.com",
+  projectId: "hurtcore-e3a43",
+  storageBucket: "hurtcore-e3a43.firebasestorage.app",
+  messagingSenderId: "753835000234",
+  appId: "1:753835000234:web:f119d43af0a8017d632bd3"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
