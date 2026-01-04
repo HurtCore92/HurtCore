d269504f11df51f6c6d4a0ee98447294542be792
HurtCore92 <e4255687@gmail.com>
2026-01-04 01:51:45 -0300

    Add files via upload

---
+++ auth.js
@@ -0,0 +1,8 @@
+const firebaseConfig = {
+  apiKey: "AIzaSyCuVqt8HcseOGGxHUjuq3gZVg4UfozLnow",
+  authDomain: "hurtcore-e3a43.firebaseapp.com",
+  projectId: "hurtcore-e3a43",
+  storageBucket: "hurtcore-e3a43.appspot.com",
+  messagingSenderId: "753835000234",
+  appId: "1:753835000234:web:f119d43af0a8017d632bd3"
+};
